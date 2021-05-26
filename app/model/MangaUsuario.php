63ac1de46e0785c3bcc92295cd5cdad9a2d998b4
TomeuPauClar <bartomeupauclar@paucasesnovescifp.cat>
2021-05-26 23:50:38 +0200

    Funció getStats editada

--- app/model/MangaUsuario.php
+++ app/model/MangaUsuario.php
@@ -21,47 +21,45 @@ class MangaUsuario extends \Illuminate\Database\Eloquent\Model
         //     WHERE `idUsuari` = " . $idUsuario . "
         // ");
 
-        // $tuplas = [];
+        $stats = [];
 
-        // $tuplas["totalCapitulosLeidos"] = MangaUsuario::where('idUsuario', $idUsuario)->sum('capitulos');
-        // $tuplas["totalVolumenesLeidos"] = MangaUsuario::where('idUsuario', $idUsuario)->sum('volumenes');
-        // $tuplas["totalMangas"] = MangaUsuario::where('idUsuario', $idUsuario)->count('idManga');
-        // $tuplas["totalMangasPorLeer"] = MangaUsuario::where('idUsuario', $idUsuario)->where('idEstado', 3)->count('idManga');
-        // $tuplas["avgNota"] = MangaUsuario::where('idUsuario', $idUsuario)->where('nota', '!=', null)->avg('nota');
-        // $tuplas["porEstado"] = MangaUsuario::selectRaw('manga_usuario.idEstado id,estado_manga_usuario.estado label, COUNT(*) value')
-        //     ->join('estado_manga_usuario', 'manga_usuario.idEstado', '=', 'estado_manga_usuario.idEstado')
-        //     ->where('manga_usuario.idUsuario', $idUsuario)
-        //     ->groupBy('manga_usuario.idEstado')
-        //     ->get();
+        $stats["totalCapitulosLeidos"] = MangaUsuario::where('idUsuario', $idUsuario)->sum('capitulos');
+        $stats["totalVolumenesLeidos"] = MangaUsuario::where('idUsuario', $idUsuario)->sum('volumenes');
+        $stats["totalMangas"] = MangaUsuario::where('idUsuario', $idUsuario)->count('idManga');
+        $stats["totalMangasPorLeer"] = MangaUsuario::where('idUsuario', $idUsuario)->where('idEstado', 3)->count('idManga');
+        $stats["avgNota"] = MangaUsuario::where('idUsuario', $idUsuario)->where('nota', '!=', null)->avg('nota');
+        $stats["porEstado"] = MangaUsuario::selectRaw('manga_usuario.idEstado id,estado_manga_usuario.estado label, COUNT(*) value')
+            ->join('estado_manga_usuario', 'manga_usuario.idEstado', '=', 'estado_manga_usuario.idEstado')
+            ->where('manga_usuario.idUsuario', $idUsuario)
+            ->groupBy('manga_usuario.idEstado')
+            ->get();
 
-        // $tuplas["lastMangaEntries"] = MangaUsuario::select(
-        //     'manga.id',
-        //     'manga.tituloPreferido',
-        //     'manga.foto',
-        //     'manga.capitulos AS totalCapitulosManga',
-        //     'manga.volumenes AS totalVolumenesManga',
-        //     'estado_manga_usuario.idEstado',
-        //     'estado_manga_usuario.estado',
-        //     'manga_usuario.nota',
-        //     'manga_usuario.volumenes',
-        //     'manga_usuario.capitulos',
-        //     'manga_usuario.updated_at',
-        // )
-        //     ->join('estado_manga_usuario', 'manga_usuario.idEstado', '=', 'estado_manga_usuario.idEstado')
-        //     ->join('manga', 'manga_usuario.idManga', '=', 'manga.id')
-        //     ->where('manga_usuario.idUsuario', $idUsuario)
-        //     ->orderBy('updated_at', 'DESC')
-        //     ->limit(3)
-        //     ->get();
+        $stats["lastMangaEntries"] = MangaUsuario::select(
+            'manga.id',
+            'manga.tituloPreferido',
+            'manga.foto',
+            'manga.capitulos AS totalCapitulosManga',
+            'manga.volumenes AS totalVolumenesManga',
+            'estado_manga_usuario.idEstado',
+            'estado_manga_usuario.estado',
+            'manga_usuario.nota',
+            'manga_usuario.volumenes',
+            'manga_usuario.capitulos',
+            'manga_usuario.updated_at',
+        )
+            ->join('estado_manga_usuario', 'manga_usuario.idEstado', '=', 'estado_manga_usuario.idEstado')
+            ->join('manga', 'manga_usuario.idManga', '=', 'manga.id')
+            ->where('manga_usuario.idUsuario', $idUsuario)
+            ->orderBy('updated_at', 'DESC')
+            ->limit(3)
+            ->get();
 
-        // $tuplas['calendar'] = ActividadUsuario::selectRaw('DATE(updated_at) AS day, COUNT(*) AS value')->where('idUsuario', $idUsuario)->groupByRaw('DATE(updated_at)')->get();
+        $stats['calendar'] = ActividadUsuario::selectRaw('DATE(updated_at) AS day, COUNT(*) AS value')->where('idUsuario', $idUsuario)->groupByRaw('DATE(updated_at)')->get();
 
-        // return $tuplas;
+        return $stats;
 
-        DB::statement("call estadisticas_usuario(:idUsuario, @estadisticas)", [ "idUsuario" => $idUsuario ]);
-        $resultado = DB::select("select @estadisticas AS stats");
-
-        return json_decode($resultado[0]->stats);
+        // DB::statement("call estadisticas_usuario(:idUsuario, @estadisticas)", ["idUsuario" => $idUsuario]);
+        // $resultado = DB::select("select @estadisticas AS stats");
     }
 
     public static function getFav(Int $idUsuario)
