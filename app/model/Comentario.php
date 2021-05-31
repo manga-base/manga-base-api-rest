e9ef99db10b0a360a0dab0dd34dfe0631bf87278
TomeuPauClar <bartomeupauclar@paucasesnovescifp.cat>
2021-05-31 23:44:00 +0200

    ta

--- app/model/Comentario.php
+++ app/model/Comentario.php
@@ -87,8 +87,8 @@ class Comentario extends \Illuminate\Database\Eloquent\Model
         }
         if (!$origen) {
             if ($comentario->idPadre) {
-                $comentarioPadre = Comentario::find($comentario->idPadre)->get();
-                //$origen = self::getOrigenComentario($comentarioPadre);
+                $comentarioPadre = Comentario::find($comentario->idPadre);
+                $origen = self::getOrigenComentario($comentarioPadre);
                 $origen->enRespuestaA = $comentarioPadre;
             }
         }
