5f0e0d1138d665503b1571cc9067a042841ce3a6
TomeuPauClar <bartomeupauclar@paucasesnovescifp.cat>
2021-05-31 23:53:55 +0200

    texto

--- app/model/Comentario.php
+++ app/model/Comentario.php
@@ -90,6 +90,7 @@ class Comentario extends \Illuminate\Database\Eloquent\Model
                 $comentarioPadre = Comentario::find($comentario->idPadre);
                 $origen = self::getOrigenComentario($comentarioPadre);
                 $enRespuestaA = Usuario::select('id', 'username', 'avatar')->where('id', $comentarioPadre->idUsuario)->get();
+                $enRespuestaA->texto = $comentarioPadre->texto;
                 $origen->enRespuestaA = $enRespuestaA[0];
             }
         }
