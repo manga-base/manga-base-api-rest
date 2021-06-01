b72e733b3770be545c581d7e8463d0e343465d9e
TomeuPauClar <bartomeupauclar@paucasesnovescifp.cat>
2021-06-01 16:25:52 +0200

    POST seguidor 1.0

--- app/routes/seguidor_route.php
+++ app/routes/seguidor_route.php
@@ -19,6 +19,7 @@ $app->group('/seguidor/', function () {
                 $seguidor = new Seguidor();
                 $seguidor->idUsuario = $decodetToken['usuario']->id;
                 $seguidor->idSeguido = $args['idUsuario'];
+                $seguidor->save();
                 $usuario = Usuario::find($args['idUsuario']);
                 return $res->withJson(Respuesta::set(true, 'Ahora sigues a ' . $usuario->username . '.'));
             } catch (Exception $error) {
