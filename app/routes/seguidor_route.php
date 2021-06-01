b1f334ff8b2178a5eceb04f66f168245f3b3e65a
TomeuPauClar <bartomeupauclar@paucasesnovescifp.cat>
2021-06-01 16:41:05 +0200

    DELETE seguidor 1.0

--- app/routes/seguidor_route.php
+++ app/routes/seguidor_route.php
@@ -19,7 +19,7 @@ $app->group('/seguidor/', function () {
             try {
                 $usuario = Usuario::find($args['idUsuario']);
                 if (!$usuario) {
-                    return $res->withJson(Respuesta::set(false, 'No existe ningun usuario con el id ' . $args['idUsuario'] . '.'));
+                    return $res->withJson(Respuesta::set(false, 'No existe ningun usuario con el id \'' . $args['idUsuario'] . '\'.'));
                 }
 
                 $posible_seguidor = Seguidor::where('idUsuario', $decodetToken['usuario']->id)->where('idSeguido', $args['idUsuario'])->get();
@@ -35,8 +35,33 @@ $app->group('/seguidor/', function () {
             } catch (Exception $error) {
                 return $res->withJson(Respuesta::set(false, $error));
             }
+        }
+    );
 
-            return $res->withJson();
+    $this->delete(
+        '{idUsuario}',
+        function ($req, $res, $args) {
+            if (!isset($args['idUsuario'])) {
+                return $res->withJson(Respuesta::set(false, 'Faltan campos.'));
+            }
+            $decodetToken = $req->getAttribute('decoded_token_data');
+            try {
+                $usuario = Usuario::find($args['idUsuario']);
+                if (!$usuario) {
+                    return $res->withJson(Respuesta::set(false, 'No existe ningun usuario con el id \'' . $args['idUsuario'] . '\'.'));
+                }
+
+                $posible_seguidor = Seguidor::where('idUsuario', $decodetToken['usuario']->id)->where('idSeguido', $args['idUsuario'])->get();
+                if (count($posible_seguidor) < 1) {
+                    return $res->withJson(Respuesta::set(false, 'No sigues a ' . $usuario->username . '.'));
+                }
+
+                $seguidor = $posible_seguidor[0];
+                $seguidor->delete();
+                return $res->withJson(Respuesta::set(true, 'Ya no sigues a ' . $usuario->username . '.'));
+            } catch (Exception $error) {
+                return $res->withJson(Respuesta::set(false, $error));
+            }
         }
     );
 });
