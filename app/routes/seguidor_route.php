28caf4692f71b319cb8b5014af296150d2d5e5c7
TomeuPauClar <bartomeupauclar@paucasesnovescifp.cat>
2021-06-01 16:34:37 +0200

    POST seguidor 3.0

--- app/routes/seguidor_route.php
+++ app/routes/seguidor_route.php
@@ -13,13 +13,21 @@ $app->group('/seguidor/', function () {
                 return $res->withJson(Respuesta::set(false, 'Faltan campos.'));
             }
             $decodetToken = $req->getAttribute('decoded_token_data');
-
+            if ($args['idUsuario'] === $decodetToken['usuario']->id) {
+                return $res->withJson(Respuesta::set(false, 'No puedes seguirte a ti mismo. (｡◕‿◕｡)'));
+            }
             try {
+                $usuario = Usuario::find($args['idUsuario']);
+
+                $posible_seguidor = Seguidor::where('idUsuario', $decodetToken['usuario']->id)->where('idSeguido', $args['idUsuario'])->get();
+                if (count($posible_seguidor) > 0) {
+                    return $res->withJson(Respuesta::set(false, 'Ya sigues a ' . $usuario->username . '.'));
+                }
+
                 $seguidor = new Seguidor();
                 $seguidor->idUsuario = $decodetToken['usuario']->id;
                 $seguidor->idSeguido = $args['idUsuario'];
                 $seguidor->save();
-                $usuario = Usuario::find($args['idUsuario']);
                 return $res->withJson(Respuesta::set(true, 'Ahora sigues a ' . $usuario->username . '.'));
             } catch (Exception $error) {
                 return $res->withJson(Respuesta::set(false, $error));
