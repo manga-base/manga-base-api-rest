21781d932174c76407840fa98f2b64dec603a5ae
TomeuPauClar <bartomeupauclar@paucasesnovescifp.cat>
2021-06-02 22:36:53 +0200

    POST contacto 3.0

--- app/routes/contacto_route.php
+++ app/routes/contacto_route.php
@@ -2,6 +2,7 @@
 
 use App\Lib\Respuesta;
 use App\Model\Contacto;
+use App\Model\FotoContacto;
 
 $app->group('/contacto/', function () {
 
@@ -16,7 +17,6 @@ $app->group('/contacto/', function () {
             $nombre = $body["nombre"];
             $email = $body["email"];
             $mensaje = $body["mensaje"];
-            $imagenes = isset($files['imagenes']) ? $files['imagenes'] : "No hay imagenes";
             try {
 
                 $contacto = new Contacto();
@@ -24,8 +24,23 @@ $app->group('/contacto/', function () {
                 $contacto->email = $email;
                 $contacto->mensaje = $mensaje;
                 $contacto->save();
-
-                return $res->withJson(Respuesta::set(true, 'Mensaje enviado correctamente.', ["contacto" => $contacto, "files" => $files, "imagenes" => $imagenes]));
+                foreach ($files as $key => $file) {
+                    $file = $file->file;
+                    if (($file <> '') && is_uploaded_file($file)) {
+                        $image_name = 'C' . $contacto->id . 'K' . $key . ".jpg";
+                        $tmp_name = $file;
+                        $dest_name = '/var/www/rest.mangabase.tk/public/upload/images/contacto/' . $image_name;
+                        if (!move_uploaded_file($tmp_name, $dest_name)) {
+                            Respuesta::set(false, 'Algo ha fallado guardando la imagen en su directorio.');
+                            return $res->withJson(Respuesta::toString());
+                        }
+                        $foto_contacto = new FotoContacto();
+                        $foto_contacto->idContacto = $contacto->id;
+                        $foto_contacto->foto = $image_name;
+                        $foto_contacto->save();
+                    }
+                }
+                return $res->withJson(Respuesta::set(true, 'Mensaje enviado correctamente.'));
             } catch (Exception $error) {
                 return $res->withJson(Respuesta::set(false, $error));
             }
