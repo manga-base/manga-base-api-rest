90d0deb0c38e11d5aa4019a2b20b4ed9ed075dfd
TomeuPauClar <bartomeupauclar@paucasesnovescifp.cat>
2021-06-03 15:55:22 +0200

    GET private-contacto test 2.1

--- app/routes/private_contacto_route.php
+++ app/routes/private_contacto_route.php
@@ -10,7 +10,7 @@ $app->group('/private-contacto/', function () {
         '',
         function ($req, $res, $args) {
             $decodetToken = $req->getAttribute('decoded_token_data');
-            if ($decodetToken['usuario']->admin === "0") {
+            if ($decodetToken['usuario']->admin === 0) {
                 return $res->withJson(Respuesta::set(false, 'No eres administrador! ಠ_ಠ'));
             }
             return $res->withJson(Respuesta::set(true, 'Hey', $decodetToken['usuario']));
