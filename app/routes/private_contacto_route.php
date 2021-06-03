d045952b3dc6edd8e5db4b1662d32a0685e05964
TomeuPauClar <bartomeupauclar@paucasesnovescifp.cat>
2021-06-03 19:41:59 +0200

    PUT private-contacto 1.0

--- app/routes/private_contacto_route.php
+++ app/routes/private_contacto_route.php
@@ -24,4 +24,25 @@ $app->group('/private-contacto/', function () {
             }
         }
     );
+
+    $this->put(
+        '{id}',
+        function ($req, $res, $args) {
+            $decodetToken = $req->getAttribute('decoded_token_data');
+            if ($decodetToken['usuario']->admin === 0) {
+                return $res->withJson(Respuesta::set(false, 'No eres administrador! ಠ_ಠ'));
+            }
+            try {
+                $mensaje = Contacto::find($args['id']);
+                if (!$mensaje) {
+                    return $res->withJson(Respuesta::set(false, 'Mensaje no encontrado'));
+                }
+                $mensaje->leido = 1;
+                $mensaje->save();
+                return $res->withJson(Respuesta::set(true, 'Mensaje marcado como leído'));
+            } catch (Exception $error) {
+                return $res->withJson(Respuesta::set(false, $error));
+            }
+        }
+    );
 });
