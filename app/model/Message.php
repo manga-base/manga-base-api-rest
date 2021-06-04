0a0c7f0ecc5690f8fc1460e1cf0a7a930149f550
TomeuPauClar <bartomeupauclar@paucasesnovescifp.cat>
2021-06-04 20:04:48 +0200

    Email verification 1.0

---
+++ app/model/Message.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Model;
+
+use App\Lib\Respuesta;
+use Exception;
+
+class Message extends \Illuminate\Database\Eloquent\Model
+{
+    protected $table = 'message';
+
+    public static function saveMail($senderEmail, $senderName, $recipientEmail, $recipientName, $subject, $data)
+    {
+        try {
+            $message = new Message();
+            $message->senderEmail = $senderEmail;
+            $message->senderName = $senderName;
+            $message->recipientEmail = $recipientEmail;
+            $message->recipientName = $recipientName;
+            $message->subject = $subject;
+            $message->status = $data['Messages'][0]['Status'];
+            $message->messageUUID = $data['Messages'][0]['To'][0]['MessageUUID'];
+            $message->messageID = $data['Messages'][0]['To'][0]['MessageID'];;
+            $message->messageHref = $data['Messages'][0]['To'][0]['MessageHref'];;
+            $message->save();
+            return Respuesta::set(true, '');
+        } catch (Exception $error) {
+            return Respuesta::set(false, $error);
+        }
+    }
+}
---
+++ app/routes/activate_route.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Lib\Respuesta;
+use App\Model\Usuario;
+
+$app->group('/activate/', function () {
+
+    $this->get(
+        '{activationCode}',
+        function ($req, $res, $args) {
+            try {
+                $posible_usuario = Usuario::where('activationCode', $args['activationCode'])->get();
+                if (count($posible_usuario) < 1) {
+                    return $res->withJson(Respuesta::set(false, 'No existe ningún usuario con este código de activación'));
+                }
+                $usuario = $posible_usuario[0];
+                $usuario->activationCode = 'Active';
+                $usuario->save();
+                return $res->withRedirect('https://mangabase.tk/email-verificado');
+            } catch (Exception $error) {
+                return $res->withJson(Respuesta::set(true, $error));
+            }
+        }
+    );
+});
