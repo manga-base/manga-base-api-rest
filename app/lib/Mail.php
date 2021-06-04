034d66ffcdffd1e1d9923887ebc8f0beffa29812
TomeuPauClar <bartomeupauclar@paucasesnovescifp.cat>
2021-06-04 20:45:30 +0200

    Mail style

--- app/lib/Mail.php
+++ app/lib/Mail.php
@@ -59,7 +59,7 @@ class Mail
                     .root {
                       width: 100%;
                       background-color: #303030;
-                      color: #fff;
+                      color: #fff !important;
                       padding: 0;
                       margin: 0;
                       font-family: -apple-system, BlinkMacSystemFont, \"Segoe UI\", Roboto, Oxygen, Ubuntu, Cantarell, \"Open Sans\", \"Helvetica Neue\", sans-serif;
